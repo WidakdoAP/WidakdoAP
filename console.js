29f77f342c22d8060c2c278f36b7eeab7e4b3184
.skid404 <152569211+WidakdoAP@users.noreply.github.com>
2025-08-24 14:21:49 +0700

    Update console.js

--- console.js
+++ console.js
@@ -8,8 +8,8 @@ const getContributions = require("./api");
   const days = await getContributions(username, token);
   const recent = days.slice(-7); // last week
 
-  let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="600" height="200" style="background:black">
-    <style> text { fill: lime; font-family: monospace; font-size: 16px; } </style>`;
+  let svg = `<svg xmlns="http://www.w3.org/2000/svg" width="600" height="200" style="background:gray">
+    <style> text { fill: white; font-family: monospace; font-size: 16px; } </style>`;
 
   recent.forEach((d, i) => {
     svg += `<text x="10" y="${20 + i * 20}">$ git commit -m "Commits: ${d.contributionCount} (${d.date})"</text>`;
